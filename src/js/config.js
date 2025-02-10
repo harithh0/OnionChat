11c803ffff51f887f9a901a4a9647527d3a157e8
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-02-10 12:49:29 -0600

    edit config

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL = "http://elzx5hscx2io6qn5wy4n6ayeg436k66xzuqrrrzltzxxuxl6h2lyqhyd.onion/api"
 export const API_URL_LOCAL_PROXY = "http://localhost:3051/?url="
-export const WS_URL = "ws://cn5mtidzpn445zyldledzrvupsmpbse3ny2dtsdwzzdv26lpj72dx6qd.onion/ws"
+export const WS_URL = "ws://jkoj2huir3zdzakrhj2tye4f2i55k2io5ug7igb37hkqclfttqxzxpad.onion:8000/ws"
 export const proxy = 'socks5h://127.0.0.1:9050';
 export const WS_PROXY = "ws://localhost:3000"
 
